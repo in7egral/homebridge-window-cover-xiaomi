02df637e3ef34211c40ef19a0c62997613b41984
in7egral <vladimir@friedapple.team>
2020-11-19 23:16:08 +0000

    fix issue with HomeBridge reboot: real curtain status, check homebridge/issues/1954

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var Service, Characteristic;
 
 module.exports = function(homebridge) {
 	console.log("homebridge API version: " + homebridge.version);
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-window-cover", "WindowCover", WindowCover);
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	// TODO: change name to homebridge-mi-curtain
+	homebridge.registerAccessory("homebridge-window-cover", "WindowCover", WindowCover);
 };
 
 
@@ -16,12 +17,12 @@ function WindowCover(log, config) {
 	this.log = log;
 	this.name = config.name || "Window cover";
 	this.id = config.id || 0;
-  this.token = config.token;
-  this.ip = config.ip;
+	this.token = config.token;
+	this.ip = config.ip;
 	this.pythonScriptPath = config.pythonScriptPath;
 	this.pythonScriptName = config.pythonScriptName;
-  this.pythonPath = config.pythonPath;
-
+	this.pythonPath = config.pythonPath;
+	this.targetNotSet = true;
 
 	// Required Characteristics
 	this.currentPosition = 100;
@@ -52,9 +53,42 @@ WindowCover.prototype = {
 	},
 	// Required
 	getCurrentPosition: function(callback) {
-		this.log("getCurrentPosition:", this.currentPosition);
 		var error = null;
-		callback(error, this.currentPosition);
+
+		if (this.pythonScriptPath !== undefined) {
+			var options = {};
+			options.args = [ this.ip, this.token ];
+			options.scriptPath = this.pythonScriptPath
+			options.pythonPath = this.pythonPath
+
+			this.log("getCurrentPosition, ask the device");
+			PythonShell.run(this.pythonScriptName, options, function (err, results) {
+				if (err) {
+					this.log("Script Error", options.scriptPath, options.args, err);
+					callback(err);
+				} else {
+					// results is an array consisting of messages collected during execution
+					//console.log('Success ! Results: %j', results);
+					if (Array.isArray(results) && results.length > 0) {
+						this.currentPosition = results[0];
+						if (this.targetNotSet) {
+							this.targetPosition = this.currentPosition;
+							this.targetNotSet = false;
+						}
+					}
+					else {
+						this.currentPosition = 100;
+					}
+					this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
+					this.log("currentPosition is now %d", this.currentPosition);
+					this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
+					callback(error, this.currentPosition); // success
+				}
+			}.bind(this));
+		} else {
+			this.log("getCurrentPosition, last known value: ", this.currentPosition);
+			callback(error, this.currentPosition);
+		}
 	},
 
 	getName: function(callback) {
@@ -64,58 +98,86 @@ WindowCover.prototype = {
 	},
 
 	getTargetPosition: function (callback) {
-		this.log("getTargetPosition :", this.targetPosition);
-		var error = null;
-		callback(error, this.targetPosition);
+		if (this.targetNotSet) {
+			if(this.pythonScriptPath !== undefined) {
+				var options = {};
+				options.args = [ this.ip, this.token ];
+				options.scriptPath = this.pythonScriptPath
+				options.pythonPath = this.pythonPath
+
+				this.log("getTargetPosition, ask the device");
+				PythonShell.run(this.pythonScriptName, options, function (err, results) {
+					if (err) {
+						this.log("Script Error", options.scriptPath, options.args, err);
+						callback(err);
+					} else {
+						// results is an array consisting of messages collected during execution
+						//console.log('Success ! Results: %j', results);
+						if (Array.isArray(results) && results.length > 0) {
+							this.targetPosition = results[0];
+						}
+						else {
+							this.targetPosition = 100;
+						}
+						this.log("targetPosition is now %d", this.targetPosition);
+						callback(error, this.targetPosition); // success
+					}
+				}.bind(this));
+				this.targetNotSet = false;
+			}
+		}
+		if (this.targetNotSet) {
+			this.log("getTargetPosition, known:", this.targetPosition);
+			var error = null;
+			callback(error, this.targetPosition);
+		}
 	},
 
 	setTargetPosition: function (value, callback) {
 		this.log("setTargetPosition from %s to %s", this.targetPosition, value);
 		this.targetPosition = value;
+		this.targetNotSet = false;
 
-		if(this.targetPosition > this.currentPosition) {
+		if (this.targetPosition > this.currentPosition) {
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.INCREASING);
-		} else if(this.targetPosition < this.currentPosition) {
+		} else if (this.targetPosition < this.currentPosition) {
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.DECREASING);
-		} else if(this.targetPosition = this.currentPosition) {
+		} else { // this.targetPosition = this.currentPosition
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
 		}		
 
-		//PYTHON 
-		if(this.pythonScriptPath !== undefined) {
-
+		if (this.pythonScriptPath !== undefined) {
 			var options = {};
-      options.args = [ this.ip, this.token, this.targetPosition ];
+			options.args = [ this.ip, this.token, this.targetPosition ];
 			options.scriptPath = this.pythonScriptPath
-      options.pythonPath = this.pythonPath
+			options.pythonPath = this.pythonPath
 
 			PythonShell.run(this.pythonScriptName, options, function (err, results) {
-			  	if (err) {
-			  		this.log("Script Error", options.scriptPath, options.args, err);
-			  	 	callback(err);
-			  	} else {
+				if (err) {
+					this.log("Script Error", options.scriptPath, options.args, err);
+					callback(err);
+				} else {
 					// results is an array consisting of messages collected during execution
-				  	console.log('Success ! Results: %j', results);
-				  	this.currentPosition = this.targetPosition;
-				  	this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
-				  	this.log("currentPosition is now %s", this.currentPosition);
+					//console.log('Success ! Results: %j', results);
+					this.currentPosition = this.targetPosition;
+					this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
+					this.log("currentPosition is now %d", this.currentPosition);
 					this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
-				  	callback(null); // success
-			  	}
+					callback(null); // success
+				}
 			}.bind(this));
 		} else {
-			//FAKE SUCCESS
 			this.log("Fake Success");
 			this.currentPosition = this.targetPosition;
 			this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
-			this.log("currentPosition is now %s", this.currentPosition);
+			this.log("currentPosition is now %d", this.currentPosition);
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
 			callback(null); // success
 		}
 	},
 
 	getPositionState: function(callback) {
-		this.log("getPositionState :", this.positionState);
+		this.log("getPositionState:", this.positionState);
 		var error = null;
 		callback(error, this.positionState);
 	},
--- plugins/curtain.py
+++ plugins/curtain.py
@@ -3,13 +3,16 @@ from miio import CurtainMiot
 
 def __main__():
     total = len(sys.argv)
-    if total < 4:
-        print ("Usage: %s <ip> <token> <position>" % sys.argv[0])
+    if total < 3:
+        print ("Usage: %s <ip> <token> [<position>]" % sys.argv[0])
         return
 
     ip=sys.argv[1]
     token=sys.argv[2]
     curtain = CurtainMiot(ip, token)
-    curtain.set_target_position(int(sys.argv[3]))
+    if total == 4:
+        curtain.set_target_position(int(sys.argv[3]))
+    else:
+        print ("%d" % curtain.status().current_position)
 
 __main__()
